6f6e7364f7c35aa8abfc9f23fb2a21fbf91d4770
millagej <millagej@icloud.com>
2019-01-18 17:59:46 -0700

    Its all good

---
+++ src/SecondPush.java
@@ -0,0 +1,15 @@
+import javax.xml.crypto.NoSuchMechanismException;
+import javax.xml.crypto.URIReferenceException;
+
+public class SecondPush {
+
+    protected static Object WebDriverManager;
+    private static URIReferenceException e;
+
+    public static void main(String[] args) {
+
+
+        class WebDriver {
+        }
+    }
+}
